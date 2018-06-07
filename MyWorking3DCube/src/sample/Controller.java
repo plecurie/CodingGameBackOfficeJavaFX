34199df2b1159a229b540686e2f017dc92e63677
Prescilla Lecurieux <prescilla.lecurieux@asn.com>
2018-06-07 16:39:51 +0200

    perform a puzzle of multi-cubes

--- MyWorking3DCube/src/sample/Controller.java
+++ MyWorking3DCube/src/sample/Controller.java
@@ -1,6 +1,79 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    //private double mousePosX, mousePosY;
+    //private double mouseOldX, mouseOldY;
+
+    private final Rotate rotateX = new Rotate(0, Rotate.X_AXIS);
+    private final Rotate rotateY = new Rotate(0, Rotate.Y_AXIS);
+
+    @FXML StackPane root;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Group puzzle = new Group();
+        double size = 400;
+        puzzle.getTransforms().addAll(rotateX, rotateY);
+
+        Box box_1 = createBox(0,0,0, size, new PhongMaterial(Color.GREEN));
+        Box box_2 = createBox(0,-0.25,0, size, new PhongMaterial(Color.CADETBLUE));
+        Box box_3 = createBox(-0.25, -0.25, 0, size, new PhongMaterial(Color.GHOSTWHITE));
+        Box box_4 = createBox(-0.25, 0, 0, size, new PhongMaterial(Color.RED));
+        Box box_5 = createBox(0, 0, -0.25, size, new PhongMaterial(Color.ORANGE));
+        Box box_6 = createBox(0, -0.25, -0.25, size, new PhongMaterial(Color.GREENYELLOW));
+        Box box_7 = createBox(-0.25, -0.25, -0.25, size, new PhongMaterial(Color.HOTPINK));
+        Box box_8 = createBox(-0.25, 0, -0.25, size, new PhongMaterial(Color.CHOCOLATE));
+
+        puzzle.getChildren().addAll(box_1,box_2,box_3,box_4,box_5,box_6,box_7,box_8);
+
+        root.getChildren().add(puzzle);
+
+        /*
+        Scene scene = new Scene(root, 800, 450, true);
+        scene.setFill(Color.BLACK);
+        scene.setCamera(new PerspectiveCamera());
+
+
+        scene.setOnMousePressed(me -> {
+            mouseOldX = me.getSceneX();
+            mouseOldY = me.getSceneY();
+        });
+        scene.setOnMouseDragged(me -> {
+            mousePosX = me.getSceneX();
+            mousePosY = me.getSceneY();
+            rotateX.setAngle(rotateX.getAngle() - (mousePosY - mouseOldY));
+            rotateY.setAngle(rotateY.getAngle() + (mousePosX - mouseOldX));
+            mouseOldX = mousePosX;
+            mouseOldY = mousePosY;
+
+        });
+        */
+
+    }
+
+    private Box createBox (double x, double y, double z, double size, PhongMaterial material) {
+        Box box = new Box(100,100,100);
+
+        box.setTranslateX(x * size);
+        box.setTranslateY(y * size);
+        box.setTranslateZ(z * size);
+        box.setMaterial(material);
+
+        return box;
+    }
 
 }
